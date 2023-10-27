399484c7e9c7237688bb696026e8e1ea16c9e664
Aierken <salayidin103@gmail.com>
2023-10-27 17:19:09 -0400

    remove keys

--- apikeys.py
+++ apikeys.py
@@ -1,8 +1,8 @@
 import os 
 
 PROJECT_ID = "radicalxtest"
-API_KEY ="ya29.a0AfB_byD11nyEAF1KziRxkxlmMMbf0-udPgq6D1VsZE-qfiYZ7unwYzb3GsjCWzjLZJoCQpkKg6NQuZDL0xcQk4h1oRn0K21XjpGnlOrnRltp3Tb79y2tRrZUeOgrdgrrfyU6pjwIi-G7Sq1Eb9K-DFClYrOnn8540oll9G1pQ9IaCgYKAUYSARASFQGOcNnC8tnI-eQ_D4TS9nBncJTZnA0178"
+API_KEY =""
 ENDPOINT_URL = f'https://us-central1-aiplatform.googleapis.com/v1/projects/{PROJECT_ID}/locations/us-central1/publishers/google/models/chat-bison:predict'
 
-FIGMA_API_KEY = "figd_ttycgLPrYWnsDKLLrLyLzjBMqRGBILVE183R8RS3"
+FIGMA_API_KEY = ""
 
